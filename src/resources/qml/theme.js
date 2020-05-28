ca90eacc45f31659ba00c0a4650eeee38dc85a54
Patricia Aas <patricia@vivaldi.com>
2020-05-28 22:45:57 +0200

    Add ; to js file

--- src/resources/qml/theme.js
+++ src/resources/qml/theme.js
@@ -1,37 +1,37 @@
-var veryLight = "#e0e2e4"
-var light = "#bccad6"
-var dark = "#8d9db6"
-var veryDark = "#667292"
+var veryLight = "#e0e2e4";
+var light = "#bccad6";
+var dark = "#8d9db6";
+var veryDark = "#667292";
 
-var veryLightPrivate = "#c6bcb6"
-var lightPrivate = "#96897f"
-var darkPrivate = "#625750"
-var veryDarkPrivate = "#686256"
+var veryLightPrivate = "#c6bcb6";
+var lightPrivate = "#96897f";
+var darkPrivate = "#625750";
+var veryDarkPrivate = "#686256";
 
 // Private Theme
-var offTheRecordColor = lightPrivate
-var offTheRecordHightlight = veryLightPrivate
-var offTheRecordLowlight = darkPrivate
-var offTheRecordIconColor = "white"
-var offTheRecordTextColor = "white"
+var offTheRecordColor = lightPrivate;
+var offTheRecordHightlight = veryLightPrivate;
+var offTheRecordLowlight = darkPrivate;
+var offTheRecordIconColor = "white";
+var offTheRecordTextColor = "white";
 
 // Public Theme
-var addressBarColor = light
-var addressBarHighlight = veryLight
-var addressBarLowlight = dark
-var addressBarIconColor = veryDark
-var textColor = "black"
+var addressBarColor = light;
+var addressBarHighlight = veryLight;
+var addressBarLowlight = dark;
+var addressBarIconColor = veryDark;
+var textColor = "black";
 
 // Common
-var addressFieldBackground = "white"
-var tabBarBackground = "white"
+var addressFieldBackground = "white";
+var tabBarBackground = "white";
 
 // Text selection
-var selectedTextColor = "white"
-var selectionColor = "steelblue"
+var selectedTextColor = "white";
+var selectionColor = "steelblue";
 
 // Icons
-var closeTabIcon = "outline-close-24px.svg"
-var backIcon = "outline-arrow_back_ios-24px.svg"
-var forwardIcon = "outline-arrow_forward_ios-24px.svg"
-var reloadIcon = "outline-refresh-24px.svg"
+var closeTabIcon = "outline-close-24px.svg";
+var backIcon = "outline-arrow_back_ios-24px.svg";
+var forwardIcon = "outline-arrow_forward_ios-24px.svg";
+var reloadIcon = "outline-refresh-24px.svg";
