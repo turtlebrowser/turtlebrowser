784682f3ef665b50c51ea2e34fd31aea014bf5f5
Patricia Aas <psmaas@gmail.com>
2020-08-31 23:36:39 +0200

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 import os
 from conans import ConanFile, CMake
 
-# Conan update 31 ubuntu-20.04
+# Conan update 40
 class ConanDependencies(ConanFile):
 
     settings = "os", "compiler", "build_type", "arch"
